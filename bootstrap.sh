ecac5614bb92576be04f428ddbf9566bbab1fd9b
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2021-03-22 17:19:51 +0100

    Add first bootstrap steps (Darwin)

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,30 @@
 # Download current trunck of kBuild (releases are pretty old).
 #
 svn co https://svn.netlabs.org/repos/kbuild/trunk/
+
+#
+# Apply patches.
+#
+cp patches/env.sh trunk/kBuild/
+
+#
+# Install missing dependencies.
+#
+brew update
+brew install autoconf automake
+
+#
+# Configure the kBuild environment.
+#
+pushd trunk
+kBuild/env.sh --full --debug-script
+
+#
+# Port and bootstrap kmk.
+#
+pushd src/kmk
+autoreconf -i
+mkdir obj && cd obj
+../configure
+popd
+popd # trunk
