1bd7f93b1b211a9a19629b2ba87fa714aa904811
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2021-04-03 18:59:44 +0200

    Improve prepare-patch

--- prepare-patch.sh
+++ prepare-patch.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+#
+# Download current trunck of kBuild (releases are pretty old)."
+#
+svn co https://svn.netlabs.org/repos/kbuild/trunk/
+
 for file in patches/**/*
 	if [ ! -d $file ] 
 	then
